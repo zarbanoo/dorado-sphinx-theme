95283006a5ae98fc7a8a04ee3b0960f98bf6b4ef
Joshua Parsons <mucephie@outlook.com>
2023-05-19 22:17:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@
 import os
 from setuptools import setup
 
-setup(use_scm_version={'write_to': os.path.join('astropy_sphinx_theme', 'version.py')})
+setup(use_scm_version={'write_to': os.path.join('dorado_sphinx_theme', 'version.py')})
